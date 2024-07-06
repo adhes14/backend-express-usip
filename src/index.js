567f6709b9e108f65058cb1cdf19ec15a3716e94
Adhemar Duran <adhemarduran@gmail.com>
2024-07-06 18:48:50 -0400

    don't force db update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import logger from './logs/logger.js';
 import { sequelize } from './database/database.js';
 
 async function main() {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
   const port = process.env.PORT ?? 3000;
   app.listen(port, () => {
     logger.info(`Server listening on port ${port}`);
